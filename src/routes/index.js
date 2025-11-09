a7fe4d1a330ff51dd4e19f180d990dae84df321a
HP657 <junlee5070@gmail.com>
2025-11-09 19:14:25 +0900

    feat: link 없는 기능 주석 처리

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const tripRoutes = require('./tripRoutes');
 const checklistRoutes = require('./checklistRoutes');
-const linkRoutes = require('./linkRoutes');
+// const linkRoutes = require('./linkRoutes');
 
 // Mount the routers for each feature
 router.use('/trips', tripRoutes);
 router.use('/checklist', checklistRoutes);
-router.use('/links', linkRoutes);
+// router.use('/links', linkRoutes);
 
 module.exports = router;
