575b70c851429943786b40732370191cc0eb96de
HP657 <junlee5070@gmail.com>
2025-11-09 18:39:34 +0900

    feat: 여행 기능 완료

--- src/routes/tripRoutes.js
+++ src/routes/tripRoutes.js
@@ -4,6 +4,7 @@ const tripController = require('./../controllers/tripController');
 
 router.post('/', tripController.createTrip);
 router.get('/:shareCode', tripController.getTripByShareCode);
+router.get('/:shareCode/details', tripController.getDetailedInfo);
 router.put('/:shareCode', tripController.updateTrip);
 router.delete('/:shareCode', tripController.deleteTrip);
 
