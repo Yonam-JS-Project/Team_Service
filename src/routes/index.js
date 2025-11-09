ff8294d17464173eb94f80b1dd58132387577e50
HP657 <junlee5070@gmail.com>
2025-11-09 18:48:39 +0900

    feat: 도메인 기능 최소화 함

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const tripRoutes = require('./tripRoutes');
-const scheduleRoutes = require('./scheduleRoutes');
 const checklistRoutes = require('./checklistRoutes');
-const memoRoutes = require('./memoRoutes');
 const linkRoutes = require('./linkRoutes');
 
 // Mount the routers for each feature
 router.use('/trips', tripRoutes);
-router.use('/schedules', scheduleRoutes);
 router.use('/checklist', checklistRoutes);
-router.use('/memos', memoRoutes);
 router.use('/links', linkRoutes);
 
 module.exports = router;
